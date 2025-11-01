5219396cbf367f1b2c20470a0bafcb878247ca45
toexpl <ir0211@outlook.com>
2025-11-01 18:32:34 +0800

    1. 初版架构

---
+++ crow/src/test/java/xyz/nanian/crow/CrowApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.nanian.crow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/xyz/nanian/owl/OwlApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.nanian.owl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
