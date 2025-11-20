d511d029746c614eba912adc68af2aa50a16761a
slnt23 <ir0211@outlook.com>
2025-11-21 00:57:04 +0800

    1. 已解决，spring-parent的3.5.7版本与knife4j 4.5.0版本不兼容，使用推荐的3.3.X与knife4j 的4.3.0版本可显示，并不受全局异常拦截影响

--- user/src/main/java/xyz/nanian/owl/user/config/GlobalExceptionHandler.java
+++ user/src/main/java/xyz/nanian/owl/user/config/GlobalExceptionHandler.java
@@ -14,18 +14,18 @@ import xyz.nanian.owl.result.ResultStatus;
  * @since 2025/11/19
  */
 
-//@RestControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//    /**
-//     * 全局通用异常处理，
-//     * @param e 异常类型，
-//     * @return 回显数据
-//     */
-//    @ExceptionHandler(value = Exception.class)
-//    public Result<String> exceptionHandler(Exception e) {
-//
-//        return Result.create(e.getMessage(), ResultStatus.FAIL);
-//    }
-//
-//}
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 全局通用异常处理，
+     * @param e 异常类型，
+     * @return 回显数据
+     */
+    @ExceptionHandler(value = Exception.class)
+    public Result<String> exceptionHandler(Exception e) {
+
+        return Result.create(e.getMessage(), ResultStatus.FAIL);
+    }
+
+}
