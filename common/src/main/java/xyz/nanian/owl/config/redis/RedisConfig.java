6aafe14a62e789fc7ad75840e3637cec1ea09b48
slnt23 <ir0211@outlook.com>
2025-12-12 00:51:29 +0800

    1. 本次更新了yml文件的布局，设置更合理的分组，以及为后续引入Nacos做基础， 2. 引入redis的配置，后续可以扩展，

---
+++ common/src/main/java/xyz/nanian/owl/config/redis/RedisConfig.java
@@ -0,0 +1,57 @@
+package xyz.nanian.owl.config.redis;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * redis配置类
+ *
+ * @author slnt23
+ * @since 2025/12/10
+ */
+
+@Configuration
+public class RedisConfig {
+
+    /**
+     * 配置序列化
+     * @param redisConnectionFactory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+//        key 与 hashKey 使用String
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+//        这里序列化与反序列化redis中的value值 使用Json代替jdk   TODO 这个类中的版本只是初版，后续还要改进
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+
+//        配置序列化方式
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(serializer);
+        redisTemplate.setHashValueSerializer(serializer);
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+
+    /**
+     * 可选的，但推荐的只操作字符串
+     * @param redisConnectionFactory
+     * @return
+     */
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+         return new StringRedisTemplate(redisConnectionFactory);
+    }
+}
---
+++ common/src/main/java/xyz/nanian/owl/utils/redis/RedisUtil.java
@@ -0,0 +1,39 @@
+package xyz.nanian.owl.utils.redis;
+
+
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * redis工具类，业务工具，对于业务层直接使用，
+ *
+ * @author slnt23
+ * @since 2025/12/10
+ */
+
+@Component
+public class RedisUtil {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void set(String key, Object value, long timeout, TimeUnit unit){
+
+        redisTemplate.opsForValue().set(key, value, timeout, unit);
+    }
+
+    public Object get(String key){
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public Boolean delete(String key){
+        return redisTemplate.delete(key);
+    }
+
+    public Boolean hasKey(String key){
+        return redisTemplate.hasKey(key);
+    }
+}
