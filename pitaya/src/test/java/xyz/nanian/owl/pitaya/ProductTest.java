c929c54f5fb77a9b1990dfab9a2a49704c2bf95e
slnt23 <ir0211@outlook.com>
2026-02-09 15:46:11 +0800

    1. 这里新多《价多多》价格管理系统， 2. 并新增watermelon 模块统一管理业务模块，这里我先将ai模块迁移，后续将pitaya模块迁移，

--- pitaya/src/test/java/xyz/nanian/owl/pitaya/ProductTest.java
+++ pitaya/src/test/java/xyz/nanian/owl/pitaya/ProductTest.java
@@ -1,18 +1,18 @@
 package xyz.nanian.owl.pitaya;
 
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.test.context.SpringBootTest;
-
-/**
- * 有关商品的测试
- *
- * @author slnt23
- * @since 2025/11/26
- */
-
-@Slf4j
-@SpringBootTest
-public class ProductTest {
-
-}
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+///**
+// * 有关商品的测试
+// *
+// * @author slnt23
+// * @since 2025/11/26
+// */
+//
+//@Slf4j
+//@SpringBootTest
+//public class ProductTest {
+//
+//}
